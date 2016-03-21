762197ec8a655d391dc90877b42ea954b8d8b8b4
CarrieShort <carrie.r.phillips@gmail.com>
2016-03-21 12:52:21 -0700

    add space troubleshooting atom crashing on repo

--- class-11-spa-routing/pair-assignment/ben-carrie/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/ben-carrie/scripts/articleController.js
@@ -1,6 +1,7 @@
 (function(module) {
   var articlesController = {};
 
+
   // TODO: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
 
   // TODO: Complete this function below that kicks off the fetching and rendering of articles, using the same
