30a000f1bfd5384062d04b28f07ad6310935171c
CarrieShort <carrie.r.phillips@gmail.com>
2016-03-21 14:19:54 -0700

    troubleshooting fixing typos

--- class-11-spa-routing/pair-assignment/ben-carrie/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/ben-carrie/scripts/articleController.js
@@ -3,14 +3,14 @@
 
 
   // DONE: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
-	Article.createTable();
+  Article.createTable();
   // DONE: Complete this function below that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   Article.fetchAll(articleView.initIndexPage);
   // DONE: Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
-	$('.tab-content').hide();
-	$('#articles').fadeIn();
+    $('.tab-content').hide();
+    $('#articles').fadeIn();
   };
 
   module.articlesController = articlesController;
